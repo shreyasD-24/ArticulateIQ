d20a401ff8c2afaae50a028e1a67af2b36423fe4
Khagesh Sharma <khageshsharma24@gmail.com>
2024-11-09 10:59:44 +0530

    Completed Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from "./Pages/Home";
+import Learning from "./Pages/Learning";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Nav from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import About from "./Pages/About";
+import Articles from "./Pages/Articles";
+import Detection from "./Pages/Detection"
+import Overalltest from "./Pages/Overalltest";
+import Coursetest from "./Pages/Coursetest";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/learning" element={<Learning />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/articles" element={<Articles />} />
+        <Route path="/detect/:number" element={<Detection />} />
+        <Route path="/overall" element={<Overalltest />} />
+        <Route path="/course/:article" element={<Coursetest />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Auth0Provider } from '@auth0/auth0-react';
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-iabl8uxrj8a06ze7.us.auth0.com"
+      clientId="y4URVw5F6jq0Aan5rsMzbyRspDpHQXdg"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
